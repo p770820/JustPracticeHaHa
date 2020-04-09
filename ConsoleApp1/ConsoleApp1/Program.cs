fd22fc2daba3183400cbc94f71ba4c7c5387d755
Peter <p770820@gmail.com>
2020-04-09 20:00:56 +0800

    加入 CreateEveryDayDirIn2020

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateEveryDayDirIn2020();
+        }
+
+        private static void CreateEveryDayDirIn2020()
+        {
+            var root = @"D:\Peter\2020";
+
+            var start = new DateTime(2020, 1, 1);
+            var end = new DateTime(2021, 1, 1);
+            for (DateTime t = start; t < end; t = t.AddDays(1))
+            {
+                var path = $"{root}\\{t.ToString("yyyy-MM-dd")}";
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+        }
+    }
+}
