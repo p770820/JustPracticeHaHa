620137618acb6d484e51b87836c144e0f998de52
Peter <p770820@gmail.com>
2020-04-10 00:27:01 +0800

    CreateRangeDayDirsInYear 參數化修改

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,18 +7,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            CreateEveryDayDirIn2020();
+            CreateRangeDayDirsInYear(2020, @"D:\Peter\2020", f => f.ToString("yyyy-MM-dd"));
         }
 
-        private static void CreateEveryDayDirIn2020()
+        private static void CreateRangeDayDirsInYear(int? year, string root, Func<DateTime, string> dirFormat)
         {
-            var root = @"D:\Peter\2020";
+            if (!year.HasValue)
+            {
+                year = DateTime.Now.Year;
+            }
 
-            var start = new DateTime(2020, 1, 1);
-            var end = new DateTime(2021, 1, 1);
+            var start = new DateTime(year.Value, 1, 1);
+            var end = new DateTime(year.Value + 1, 1, 1);
+
             for (DateTime t = start; t < end; t = t.AddDays(1))
             {
-                var path = $"{root}\\{t.ToString("yyyy-MM-dd")}";
+                var path = $"{root}\\{dirFormat}";
                 if (!Directory.Exists(path))
                 {
                     Directory.CreateDirectory(path);
