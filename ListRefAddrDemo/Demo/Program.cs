66390ea2afdaa3732dbc46f86023bfb1fdc3f1a0
Peter <p770820@gmail.com>
2020-06-12 00:50:40 +0800

    參考問題

---
+++ ListRefAddrDemo/Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "Peter" };
+            Console.WriteLine(JsonSerializer.Serialize(list));
+
+            Proc(ref list);
+
+            Console.WriteLine(JsonSerializer.Serialize(list));
+            Console.WriteLine("Hello world!");
+        }
+
+
+        private static void Proc(ref List<string> list)
+        {
+            //list.Add("William");
+
+            var output = new List<string>() { "William" };
+            list = output;
+        }
+    }
+}
