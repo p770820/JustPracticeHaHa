4897dd18480b263372b44758c11726951af837c1
Peter <p770820@gmail.com>
2020-06-12 00:50:24 +0800

    API Error Handle 相關

---
+++ ErrorHandleSample/Controllers/ErrorCodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandleSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorCodeController : ControllerBase
+    {
+        public IActionResult Get(string code)
+        {
+            // logger
+            return BadRequest($"Error, status code: {code}");
+        }
+    }
+}
