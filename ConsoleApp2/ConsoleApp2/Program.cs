d82a719fdab36f6eed92353b076397e28379ead8
Peter <p770820@gmail.com>
2020-06-12 00:50:02 +0800

    正則 判斷 在兩個字串中間 判斷 在某個字串之後

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = $@"www.xxx.com/group/{ Guid.NewGuid() }/report/{ Guid.NewGuid() }";
+
+            var groupRegex = new Regex(@"(?<=group/)(.*?)(?=/report)");
+            var reportRegex = new Regex(@"(?<=report/)(.*)");
+
+
+            Console.WriteLine(url);
+            Console.WriteLine(groupRegex.Match(url).Value);
+            Console.WriteLine(reportRegex.Match(url).Value);
+        }
+    }
+}
