64266c2aedb175f86dc55a333a30c69d27c13b36
Peter <p770820@gmail.com>
2021-01-01 22:42:41 +0800

    彼得計算機 Add first + second 單元測試練習

---
+++ PeterCalculator/PeterCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterCalculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ PeterCalculator/PeterCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeterCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeterCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            int actual = 2;
+
+            // Act
+            var expected = calculator.Add(1, 1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
