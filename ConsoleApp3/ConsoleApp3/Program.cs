a9dc4c69553458e72e22948b3854cdc86aef744c
Peter <p770820@gmail.com>
2021-01-01 22:07:07 +0800

    ConsoleApp3 研究怎麼從 try leave (幫雯雯研究)

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "A";
+
+            try
+            {
+
+                //...
+                result += "B";
+            }
+            catch (MyException myEx)
+            {
+                
+            }
+            catch
+            {
+                result += "C";
+                throw;
+            }
+            finally
+            {
+                result += "D";
+            }
+
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public class MyException: Exception
+    {
+
+    }
+}
