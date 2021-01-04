1c335132b9cfd6e110a6965be850f348b03d74ef
Peter <p770820@gmail.com>
2021-01-04 15:18:49 +0800

    Barcode，建立 ZXingNetController ，安裝 ZXingNet 套件。

---
+++ PeterBarcodeDemo/PeterBarcodeDemo/Controllers/ZXingNetController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeterBarcodeDemo.Controllers
+{
+    // https://github.com/micjahn/ZXing.Net
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ZXingNetController : Controller
+    {
+        // GET: ZXingNet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
