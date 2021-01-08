4e1eb730e08b7480ce4859e40462de582cd47cda
Peter <p770820@gmail.com>
2021-01-09 01:17:00 +0800

    網球，重構，把 if 重構成 dictionary。

--- PeterTennisDemo/TennisGame.Library/TennisGame.cs
+++ PeterTennisDemo/TennisGame.Library/TennisGame.cs
@@ -1,26 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TennisGame.Library
 {
     public class TennisGame
     {
         private int _firstPlayerScoreTimes = 0;
+        private Dictionary<int, string> _scoreDic = new Dictionary<int, string>();
         public TennisGame()
         {
+            this._scoreDic.Add(1, "Fifteen Love");
+            this._scoreDic.Add(2, "Thirty Love");
         }
 
         public string Score()
         {
-            if(this._firstPlayerScoreTimes == 1)
+            if(this._firstPlayerScoreTimes > 0)
             {
-                return "Fifteen Love";
+                return this._scoreDic[this._firstPlayerScoreTimes];
             }
-
-            if(this._firstPlayerScoreTimes == 2)
-            {
-                return "Thirty Love";
-            }
-
             return "Love All";
         }
 
