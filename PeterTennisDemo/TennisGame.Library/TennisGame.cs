1f4803cccf01873c84dd443b43e84ca502197f90
Peter <p770820@gmail.com>
2021-01-08 23:05:33 +0800

    網球，實作 FirstPlayerScore()，通過 Fifteen_Love 測試案例。

--- PeterTennisDemo/TennisGame.Library/TennisGame.cs
+++ PeterTennisDemo/TennisGame.Library/TennisGame.cs
@@ -4,18 +4,23 @@ namespace TennisGame.Library
 {
     public class TennisGame
     {
+        private int _firstPlayerScore = 0;
         public TennisGame()
         {
         }
 
         public string Score()
         {
+            if(this._firstPlayerScore > 0)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
         }
 
         public void FirstPlayerScore()
         {
-            throw new NotImplementedException();
+            this._firstPlayerScore++;
         }
     }
 }
