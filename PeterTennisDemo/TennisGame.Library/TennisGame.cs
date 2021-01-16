9283232f5b2830092ad412307d1d9b149cbd6919
Peter <p770820@gmail.com>
2021-01-17 02:25:43 +0800

    網球，重構。

--- PeterTennisDemo/TennisGame.Library/TennisGame.cs
+++ PeterTennisDemo/TennisGame.Library/TennisGame.cs
@@ -22,46 +22,58 @@ namespace TennisGame.Library
 
         public string Score()
         {
-            var leadPlayerName = this._firstPlayerScoreTimes > this._secondPlayerScoreTimes ? "FirstPlayer" : "SecondPlayer";
-            var leadScoreTimes = this._firstPlayerScoreTimes > this._secondPlayerScoreTimes ? this._firstPlayerScoreTimes : this._secondPlayerScoreTimes;
-            var behindScoreTimes = this._firstPlayerScoreTimes > this._secondPlayerScoreTimes ? this._secondPlayerScoreTimes : this._firstPlayerScoreTimes;
-
-            if (IsSameScore())
-            {
-                return IsDesue() ? "Deuse" : $"{ _scoreLookup[this._secondPlayerScoreTimes] } All";
-            }
-
-            if (IsAdv(leadScoreTimes))
-            {
-                return $"{leadPlayerName} { (IsWin(leadScoreTimes, behindScoreTimes) ? "Win" : "Adv") }";
-            }
-
-            if (this._firstPlayerScoreTimes > 0)
-            {
-                return $"{ _scoreLookup[this._firstPlayerScoreTimes] } Love";
-            }
-
-            if (this._secondPlayerScoreTimes > 0)
-            {
-                return $"Love { _scoreLookup[this._secondPlayerScoreTimes] }";
-            }
-
-            return "";
+            return IsSameScore() ? (IsDesue() ? DeuseResult() : SameScoreResult())
+                : (IsAdv() ? AdvOrWinResult() : DifferentScoreResult());
         }
 
-        private static bool IsWin(int leadScoreTimes, int behindScoreTimes)
+        private int GetBehindScoreTimes()
+        {
+            return IsFirstPlayerLeadSecondPlayer() ? this._secondPlayerScoreTimes : this._firstPlayerScoreTimes;
+        }
+
+        private int GetLeadScoreTimes()
+        {
+            return IsFirstPlayerLeadSecondPlayer() ? this._firstPlayerScoreTimes : this._secondPlayerScoreTimes;
+        }
+
+        private bool IsFirstPlayerLeadSecondPlayer()
+        {
+            return this._firstPlayerScoreTimes > this._secondPlayerScoreTimes;
+        }
+
+        private string SameScoreResult()
+        {
+            return $"{ _scoreLookup[this._secondPlayerScoreTimes] } All";
+        }
+
+        private string DeuseResult()
+        {
+            return "Deuse";
+        }
+
+        private string DifferentScoreResult()
+        {
+            return $"{ _scoreLookup[this._firstPlayerScoreTimes] } { _scoreLookup[this._secondPlayerScoreTimes] }";
+        }
+
+        private string AdvOrWinResult()
+        {
+            return $"{ GetLeadPlayerName() } { (IsWin(GetLeadScoreTimes(), GetBehindScoreTimes()) ? "Win" : "Adv") }";
+        }
+
+        private string GetLeadPlayerName()
+        {
+            return (IsFirstPlayerLeadSecondPlayer() ? "FirstPlayer" : "SecondPlayer");
+        }
+
+        private bool IsWin(int leadScoreTimes, int behindScoreTimes)
         {
             return (leadScoreTimes == 4 && behindScoreTimes == 0) || (leadScoreTimes - behindScoreTimes == 2);
         }
 
-        private bool DifferentScore(int leadScoreTimes)
+        private bool IsAdv()
         {
-            return !IsSameScore() && leadScoreTimes > 0;
-        }
-
-        private bool IsAdv(int leadScoreTimes)
-        {
-            return !IsSameScore() && leadScoreTimes > 3;
+            return !IsSameScore() && GetLeadScoreTimes() > 3;
         }
 
         private bool IsDesue()
