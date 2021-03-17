ce8fac0a9c4f2d12a6b70108c1c3d86cfb49bf32
Peter <p770820@gmail.com>
2021-03-17 13:04:54 +0800

    XSS Test

---
+++ WebApplication1/WebApplication1/Controllers/XSSTESTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class XSSTESTController : Controller
+    {
+        // GET: XSSTEST
+        public ActionResult Index(string v)
+        {
+            ViewBag.QueryString = v;
+            return View();
+        }
+    }
+}
