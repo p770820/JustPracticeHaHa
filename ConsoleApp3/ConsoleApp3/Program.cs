3043b9c55c586e1ae487357e27b12bf70ef2dfcb
Peter <p770820@gmail.com>
2021-05-03 15:28:58 +0800

    取得 KBF 某個泳道的卡片。

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +10,47 @@ namespace ConsoleApp3
 {
     class Program
     {
+        static string token = "";
+        static string columnId = "";
+
         static void Main(string[] args)
+        {
+            var column = new List<Column>();
+            column = GetColumn();
+
+            if (!column.Any())
+                return;
+
+            foreach (var task in column[0].tasks)
+            {
+                foreach (var date in task.dates)
+                {
+                }
+            }
+            Console.WriteLine(column);
+        }
+
+        /// <summary>
+        /// 取得某泳道的卡片 
+        /// </summary>
+        /// <returns></returns>
+        private static List<Column> GetColumn()
+        {
+            List<Column> column;
+            WebClient client = new WebClient();
+            var url = $@"https://kanbanflow.com/api/v1/tasks?apiToken={ token }&columnId={ columnId }";
+
+            byte[] data = client.DownloadData(url);
+            string json = Encoding.UTF8.GetString(data);
+
+            Console.WriteLine(json);
+
+            column = JsonConvert.DeserializeObject<List<Column>>(json);
+
+            return column;
+        }
+
+        private static void TryOutDemo()
         {
             string result = "A";
 
@@ -20,7 +62,7 @@ namespace ConsoleApp3
             }
             catch (MyException myEx)
             {
-                
+                throw myEx;
             }
             catch
             {
