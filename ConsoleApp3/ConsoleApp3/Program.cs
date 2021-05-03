ca7467ec4988fd89fbc3117c92877941f944a177
Peter <p770820@gmail.com>
2021-05-03 17:58:55 +0800

    KBF 更新過期日期。

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,40 +13,78 @@ namespace ConsoleApp3
         static string token = "";
         static string columnId = "";
 
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             var column = new List<Column>();
-            column = GetColumn();
+            column = await GetColumnAsync();
 
             if (!column.Any())
                 return;
 
             foreach (var task in column[0].tasks)
             {
-                foreach (var date in task.dates)
+                await UpdateTaskDate(task);
+            }
+            Console.WriteLine("Finish!");
+            Console.ReadLine();
+        }
+
+        private static async System.Threading.Tasks.Task UpdateTaskDate(Task task)
+        {
+            var url = $@"https://kanbanflow.com/api/v1/tasks/{ task._id }/dates?apiToken={ token }";
+
+            Console.WriteLine($"{task._id}  { task.name }");
+            Console.WriteLine(" ============ task update date start =============");
+            foreach (var date in task.dates)
+            {
+                if (date.dueTimestampLocal > DateTime.Now)
                 {
+                    Console.WriteLine("not due date!");
+                    continue;
+                }
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Date newDate = new Date();
+                    newDate.targetColumnId = date.targetColumnId;
+                    newDate.status = "active";
+                    newDate.dateType = "dueDate";
+
+                    var newTime = new DateTime(
+                        date.dueTimestampLocal.Year, 
+                        5,// date.dueTimestampLocal.Month, 
+                        date.dueTimestampLocal.Day, 
+                        date.dueTimestampLocal.Hour, 
+                        date.dueTimestampLocal.Minute, 
+                        0);
+                    newDate.dueTimestamp = newTime.ToUniversalTime();
+                    newDate.dueTimestampLocal = newTime.ToLocalTime();
+
+                    string json = JsonConvert.SerializeObject(newDate);
+                    HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    Console.WriteLine(response.StatusCode);
                 }
             }
-            Console.WriteLine(column);
+            Console.WriteLine(" ============ task update date end =============");
         }
 
         /// <summary>
         /// 取得某泳道的卡片 
         /// </summary>
         /// <returns></returns>
-        private static List<Column> GetColumn()
+        private static async Task<List<Column>> GetColumnAsync()
         {
-            List<Column> column;
-            WebClient client = new WebClient();
+            List<Column> column = new List<Column>();
             var url = $@"https://kanbanflow.com/api/v1/tasks?apiToken={ token }&columnId={ columnId }";
 
-            byte[] data = client.DownloadData(url);
-            string json = Encoding.UTF8.GetString(data);
-
-            Console.WriteLine(json);
-
-            column = JsonConvert.DeserializeObject<List<Column>>(json);
-
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                column = JsonConvert.DeserializeObject<List<Column>>(responseBody);
+            }
             return column;
         }
 
@@ -79,7 +117,7 @@ namespace ConsoleApp3
         }
     }
 
-    public class MyException: Exception
+    public class MyException : Exception
     {
 
     }
