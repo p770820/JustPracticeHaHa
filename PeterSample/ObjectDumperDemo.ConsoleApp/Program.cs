7a34980f03df16e92b2a40d46d37c08f86127a7b
Peter <p770820@gmail.com>
2021-06-25 12:29:39 +0800

    Revert "Object Dumper，測試物件的屬性為物件集合。"
    This reverts commit 4fd943a89d331b60f219455891019cce36a2e930.

--- PeterSample/ObjectDumperDemo.ConsoleApp/Program.cs
+++ PeterSample/ObjectDumperDemo.ConsoleApp/Program.cs
@@ -10,13 +10,7 @@ namespace ObjectDumperDemo.ConsoleApp
             var persons = new List<Person>
             {
                 new Person { Name = "John", Age = 20,
-                    NickName = new string[] { "猛John", "JJ" },
-                    FamilyMembers = new List<Person>() { 
-                        new Person(){ Name = "JohnPa", Age = 50, NickName = new string[]{ "JPapa" } },
-                        new Person(){ Name = "JohnMa", Age = 45, NickName = new string[]{ "JMama" } },
-                        new Person(){ Name = "JohnOni", Age = 21, NickName = new string[]{ "JOni" } },
-                    }
-                },
+                    NickName = new string[] { "猛John", "JJ" } },
                 new Person { Name = "Thomas", Age = 30,
                     NickName = new string[] { "滅霸", "響指哥" }},
             };
