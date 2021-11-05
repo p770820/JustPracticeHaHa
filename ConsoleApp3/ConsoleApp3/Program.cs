cb7b29492907c919757efe5e4e007518f9de718d
Peter <p770820@gmail.com>
2021-11-06 01:39:12 +0800

    Kanbanflow 日記卡片做額外處理。

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp3
     class Program
     {
         static string token = "";
-        static string columnId = "";
+        static string columnId = ""; // 願望池
 
         static async System.Threading.Tasks.Task Main(string[] args)
         {
@@ -23,11 +23,24 @@ namespace ConsoleApp3
 
             foreach (var task in column[0].tasks)
             {
-                await UpdateTaskDate(task);
+                //if (task.name.Contains("整理"))
+                //{
+                //    await UpdateTaskDateOfClean(task);
+                //}
+
+                if (task.color == "magenta") // 日記
+                {
+                    await UpdateTaskDateOfDiary(task);
+                }
+                else
+                {
+                    //await UpdateTaskDate(task);
+                }
             }
             Console.WriteLine("Finish!");
             Console.ReadLine();
         }
+
 
         private static async System.Threading.Tasks.Task UpdateTaskDate(Task task)
         {
@@ -51,11 +64,100 @@ namespace ConsoleApp3
                     newDate.dateType = "dueDate";
 
                     var newTime = new DateTime(
-                        date.dueTimestampLocal.Year, 
-                        5,// date.dueTimestampLocal.Month, 
-                        date.dueTimestampLocal.Day, 
+                        DateTime.Now.Year, //date.dueTimestampLocal.Year, 
+                        DateTime.Now.Month, //date.dueTimestampLocal.Month, 
+                        DateTime.Now.Day, //date.dueTimestampLocal.Day, 
                         date.dueTimestampLocal.Hour, 
                         date.dueTimestampLocal.Minute, 
+                        0);
+
+                    newTime = newTime.AddDays((int)newTime.DayOfWeek);
+
+                    newDate.dueTimestamp = newTime.ToUniversalTime();
+                    newDate.dueTimestampLocal = newTime.ToLocalTime();
+
+                    string json = JsonConvert.SerializeObject(newDate);
+                    HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    Console.WriteLine(response.StatusCode);
+                }
+            }
+            Console.WriteLine(" ============ task update date end =============");
+        }
+
+        private static async System.Threading.Tasks.Task UpdateTaskDateOfDiary(Task task)
+        {
+            var url = $@"https://kanbanflow.com/api/v1/tasks/{ task._id }/dates?apiToken={ token }";
+
+            Console.WriteLine($"{task._id}  { task.name }");
+            Console.WriteLine(" ============ task update date start =============");
+            foreach (var date in task.dates)
+            {
+                if (date.dueTimestampLocal > DateTime.Now)
+                {
+                    Console.WriteLine("not due date!");
+                    continue;
+                }
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Date newDate = new Date();
+                    newDate.targetColumnId = date.targetColumnId;
+                    newDate.status = "active";
+                    newDate.dateType = "dueDate";
+
+                    int i = 1;
+                    int.TryParse(task.name.Substring(task.name.Length - 2, 2), out i);
+
+                    var newTime = new DateTime(
+                        date.dueTimestampLocal.Year,
+                        12,//date.dueTimestampLocal.Month,
+                        i <= 0 ? 1 : i,//date.dueTimestampLocal.Day,
+                        date.dueTimestampLocal.Hour,
+                        date.dueTimestampLocal.Minute,
+                        0);
+                    newDate.dueTimestamp = newTime.ToUniversalTime();
+                    newDate.dueTimestampLocal = newTime.ToLocalTime();
+
+                    string json = JsonConvert.SerializeObject(newDate);
+                    HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+                    Console.WriteLine(response.StatusCode);
+                }
+            }
+            Console.WriteLine(" ============ task update date end =============");
+        }
+
+        private static async System.Threading.Tasks.Task UpdateTaskDateOfClean(Task task)
+        {
+            var url = $@"https://kanbanflow.com/api/v1/tasks/{ task._id }/dates?apiToken={ token }";
+
+            Console.WriteLine($"{task._id}  { task.name }");
+            Console.WriteLine(" ============ task update date start =============");
+            foreach (var date in task.dates)
+            {
+                //if (date.dueTimestampLocal > DateTime.Now)
+                //{
+                //    Console.WriteLine("not due date!");
+                //    continue;
+                //}
+
+                using (HttpClient client = new HttpClient())
+                {
+                    Date newDate = new Date();
+                    newDate.targetColumnId = date.targetColumnId;
+                    newDate.status = "active";
+                    newDate.dateType = "dueDate";
+
+                    int i = 1;
+                    int.TryParse(task.name.Substring(task.name.Length - 2, 2), out i);
+                    
+                    var newTime = new DateTime(
+                        date.dueTimestampLocal.Year,
+                        7,//date.dueTimestampLocal.Month,
+                        i <= 0 ? 1 : i,//date.dueTimestampLocal.Day,
+                        date.dueTimestampLocal.Hour,
+                        date.dueTimestampLocal.Minute,
                         0);
                     newDate.dueTimestamp = newTime.ToUniversalTime();
                     newDate.dueTimestampLocal = newTime.ToLocalTime();
